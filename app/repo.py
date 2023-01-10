38acf3bad42a653e834612e43f6d0ad88f4ac641
Piotr Patrzyk <piotr@patrzyk.me>
2023-01-10 17:54:50 +0100

    reading files from git history

--- app/repo.py
+++ app/repo.py
@@ -11,16 +11,6 @@ def get_conn_str(db):
     var_name = f"GITBI_{db.upper()}_CONN"
     return os.environ[var_name]
 
-def get_file_content(path):
-    """
-    """
-    # TODO think if this should be last git version or last file on disk
-    # https://stackoverflow.com/a/9684612
-    with open(path, "r") as f:
-        content = f.read()
-        assert re.sub("\s", "", content), "File is empty"
-    return content
-
 def get_query(db, file):
     """
     """
@@ -39,3 +29,38 @@ def list_sources():
     db_dirs = {db_dir for db_dir in os.scandir(DIR) if db_dir.is_dir() and db_dir.name != ".git"}
     sources = {db_dir.name: tuple(el.name for el in os.scandir(db_dir) if el.is_file()) for db_dir in db_dirs}
     return sources
+
+def get_file_content(path):
+    """
+    """
+    with open(path, "r") as f:
+        content = f.read()
+        assert re.sub("\s", "", content), "File is empty"
+    return content
+
+# https://stackoverflow.com/a/9684612
+def get_tree_objects(tree):
+    """
+    """
+    return tuple(get_tree_objects_generator(tree, prefix=""))
+
+def get_tree_objects_generator(tree, prefix=""):
+    """
+    """
+    for obj in tree:
+        if obj.type_str == "blob":
+            yield os.path.join(prefix, obj.name)
+        elif obj.type_str == "tree":
+            new_prefix = os.path.join(prefix, obj.name)
+            for entry in get_tree_objects_generator(obj, new_prefix):
+                yield entry
+
+def get_file_content_git(tree, path):
+    """
+    """
+    #TODO path is relative without DIR, unify later
+    blob = tree / path
+    return blob.data.decode("UTF-8")
+
+# head = REPO.revparse_single('HEAD')
+# commits = tuple(commit for commit in REPO.walk(REPO.head.target))
