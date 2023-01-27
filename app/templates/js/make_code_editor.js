08ef5b1ad99d9a6e77be09b4fe8d83a72e15ba0a
Piotr Patrzyk <piotr@patrzyk.me>
2023-01-27 14:45:31 +0100

    js: generate share link, some frontend fixes and todos

--- app/templates/js/make_code_editor.js
+++ app/templates/js/make_code_editor.js
@@ -10,12 +10,23 @@ let vega_editor = document.getElementById("vega-editor");
 window.query_jar = CodeJar(query_editor, hljs.highlightElement);
 window.vega_jar = CodeJar(vega_editor, hljs.highlightElement);
 function query_format() {
-    var include_vega = document.getElementById("include-vega").checked
-    var file_name = document.getElementById("file-name").value.trim()
-    var data = {query: query_jar.toString(), file: file_name}
+    var include_vega = document.getElementById("include-vega").checked;
+    var file_name = document.getElementById("file-name").value.trim();
+    var data = {query: query_jar.toString(), file: file_name};
     if (include_vega) {
-        data["vega"] = vega_jar.toString()
+        data["vega"] = vega_jar.toString();
     }
     return JSON.stringify(data)
 }
 window.query_format = query_format;
+function generate_link() {
+    var data = JSON.parse(query_format());
+    var path = window.location.pathname.split("/").slice(0, 3).join("/");
+    var query = "?";
+    for (let [key, value] of Object.entries(data)) {
+        query += `${encodeURIComponent(key)}=${encodeURIComponent(value)}&`;
+    }
+    var url = `${window.location.origin}${path}${query}`;
+    alert(url);
+}
+window.generate_link = generate_link;
--- app/templates/js/make_table.js
+++ app/templates/js/make_table.js
@@ -1,16 +1,22 @@
 var table = document.getElementById("results-table");
 var data_table = new simpleDatatables.DataTable(table, {
     perPageSelect: [10, 25, 50, 100],
-    classes: {bottom: "grid", top: "grid"},
+    classes: {bottom: "grid", top: "grid", selector: "no-margin",},
     layout: {
         top: "{select}{search}",
         bottom: "{pager}{info}"
     },
 });
-document.getElementById('csv-button').addEventListener("click", () => {
+var search_button = document.getElementsByClassName("datatable-search")[0];
+var csv_button = document.createElement("div"); 
+csv_button.setAttribute("class", "secondary")
+csv_button.setAttribute("role", "button")
+csv_button.innerText = "Export CSV";
+csv_button.addEventListener("click", () => {
     simpleDatatables.exportCSV(data_table, {
         download: true,
         lineDelimiter: "\n",
         columnDelimiter: ";"
     })
 });
+search_button.insertAdjacentElement('afterend', csv_button);
